ca7208a65fdf2622989fa23e8c6fc8303af2a1d6
iguardines@gmail.com <iguardines@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-06-10 15:42:28 +0000

    Creada la clase principal de la aplicacion y agregados atributos a la clase usuario.

---
+++ trunk/aterrizar.com/src/ar/com/aterrizar/modelo/AterrizarApp.java
@@ -0,0 +1,28 @@
+package ar.com.aterrizar.modelo;
+
+import java.util.Collection;
+//import java.util.List;
+
+public class AterrizarApp {
+
+	protected static AterrizarApp instance;
+	protected Collection<Usuario> usuarios;
+	//protected List<Aerolinea> aerolineas;
+	
+	private AterrizarApp() {
+	}
+
+	public static AterrizarApp getInstance(){
+		if(instance == null)
+			instance = new AterrizarApp();
+		return instance;
+	}
+	
+	public void registarUsuario(String nombre,String apellido, String dni){
+		Usuario nuevoUsuario = new Usuario();
+		nuevoUsuario.setNombre(nombre);
+		nuevoUsuario.setAPellido(apellido);
+		nuevoUsuario.setDNI(dni);
+		
+	}
+}
