41d5bf7207601103a0d7a93d329856efd2f466dc
iguardines@gmail.com <iguardines@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-06-10 15:42:28 +0000

    Creada la clase principal de la aplicacion y agregados atributos a la clase usuario.

--- aterrizar.com/src/ar/com/aterrizar/modelo/Usuario.java
+++ aterrizar.com/src/ar/com/aterrizar/modelo/Usuario.java
@@ -1,8 +1,45 @@
 package ar.com.aterrizar.modelo;
 
+import java.util.List;
+
 import ar.com.aterrizar.negocio.Calculable;
 
 public class Usuario {
+
+	protected String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public List<Busqueda> getBusquedas() {
+		return busquedas;
+	}
+
+	protected String apellido;
+	protected String dni;
 	protected Calculable tipoDeUsuario;
-	//protected <List>Busqueda busquedas;
+
+	protected List<Busqueda> busquedas;
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+
+	}
+
+	public void setAPellido(String apellido) {
+		this.apellido = apellido;
+
+	}
+
+	public void setDNI(String dni) {
+		this.dni = dni;
+	}
 }
