0bafb4add441e3b9d5ade49ab69f34bb227e8a0f
iguardines@gmail.com <iguardines@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-06-10 18:16:19 +0000

    agregados en el objeto busqueda.

--- aterrizar.com/test/ar/com/aterrizar/modelo/AerolineaLanchitaInterfaceTest.java
+++ aterrizar.com/test/ar/com/aterrizar/modelo/AerolineaLanchitaInterfaceTest.java
@@ -3,6 +3,8 @@ package ar.com.aterrizar.modelo;
 import java.util.Arrays;
 import java.util.List;
 
+import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,13 +13,14 @@ import com.lanchita.AerolineaLanchita;
 public class AerolineaLanchitaInterfaceTest {
 
 	protected AerolineaLanchita  aerolineaLanchita;
-	
+	protected Busqueda busquedaBALA20121010;
 	
 	
 	@Before
 	public void setUp(){
 		
 		aerolineaLanchita = AerolineaLanchita.getInstance();
+		busquedaBALA20121010= new Busqueda("BUE", "20121010",null, "LA", null, null);
 	}
 	
 	
@@ -30,6 +33,20 @@ public class AerolineaLanchitaInterfaceTest {
 				System.out.println(asiento);
 			}
 		}
+		
+		System.out.println(aerolineaLanchita.asientosDisponibles("BUE", "20121010",null, "LA", null, null));
+	}
+	
+	public void obtenerAsientoApartirDeUnaBusquedaConBue20121010La(){
+		String origen = busquedaBALA20121010.getOrigen();
+		
+		String destino = busquedaBALA20121010.getDestino();
+		String fechaSalida = busquedaBALA20121010.getFechaSalida();
+		String horaSalida = busquedaBALA20121010.getHoraSalida();
+		String fechaLlegada = busquedaBALA20121010.getFechaLlegada();
+		String horaLlegada = busquedaBALA20121010.getHoraLlegada();		
+
+		Assert.assertNotNull(aerolineaLanchita.asientosDisponibles(origen, destino, fechaSalida, horaSalida, fechaLlegada, horaLlegada));
 	}
 	
 	
