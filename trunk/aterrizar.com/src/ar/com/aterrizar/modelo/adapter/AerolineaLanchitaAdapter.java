1c63ec4e82c2f11100384c703f44ce9614d5ca9d
iguardines@gmail.com <iguardines@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-06-10 18:47:33 +0000

    Agregado de la clase abstracta Aerolinea para poder implementar el adaptador.

--- trunk/aterrizar.com/src/ar/com/aterrizar/modelo/adapter/AerolineaLanchitaAdapter.java
+++ trunk/aterrizar.com/src/ar/com/aterrizar/modelo/adapter/AerolineaLanchitaAdapter.java
@@ -1,21 +1,61 @@
 package ar.com.aterrizar.modelo.adapter;
 
+//import java.util.ArrayList;
+import java.util.List;
+
+import ar.com.aterrizar.modelo.Aerolinea;
+import ar.com.aterrizar.modelo.Asiento;
 import ar.com.aterrizar.modelo.Busqueda;
 
 import com.lanchita.AerolineaLanchita;
 
-public class AerolineaLanchitaAdapter {
-	
+public class AerolineaLanchitaAdapter extends Aerolinea {
+
 	protected Integer porcentajePorCompania;
 	protected AerolineaLanchita aerolinea;
-	
-	public AerolineaLanchitaAdapter(){
+
+	public AerolineaLanchitaAdapter() {
 		aerolinea = AerolineaLanchita.getInstance();
 	}
-	
-	/*String[][] com.lanchita.AerolineaLanchita.asientosDisponibles(String origen, String destino, String fechaSalida, String horaSalida, String fechaLlegada, String horaLlegada)*/
-	public void buscarAsiento(Busqueda unaBusqueda){
-	//	aerolinea.asientosDisponibles(origen, destino, fechaSalida, horaSalida, fechaLlegada, horaLlegada)
-	}	
+
+	/*
+	 * String[][] com.lanchita.AerolineaLanchita.asientosDisponibles(String
+	 * origen, String destino, String fechaSalida, String horaSalida, String
+	 * fechaLlegada, String horaLlegada)
+	 */
+	public List<Asiento> buscarAsientos(Busqueda unaBusqueda) {
+
+		//TODO falta implementar la logica, agregar en asiento el parser para la construccion de asciento a partir de una lista de strings
+		//		String origen = unaBusqueda.getOrigen();
+//
+//		String destino = unaBusqueda.getDestino();
+//		String fechaSalida = unaBusqueda.getFechaSalida();
+//		String horaSalida = unaBusqueda.getHoraSalida();
+//		String fechaLlegada = unaBusqueda.getFechaLlegada();
+//		String horaLlegada = unaBusqueda.getHoraLlegada();
+//
+//		int indexy = 0;
+//		String[][] datosAsientos = this.aerolinea.asientosDisponibles(origen,
+//				destino, fechaSalida, horaSalida, fechaLlegada, horaLlegada);
+//		if (datosAsientos == null)
+//			return new ArrayList<Asiento>(0);
+//		
+//		ArrayList<Asiento> asientos = new ArrayList<Asiento>();
+//		
+//		for (int indexx = 0; indexx < datosAsientos.length; indexx++) {
+//			System.out.println(datosAsientos[indexx][indexy]);
+//		}
+
+		throw new RuntimeException("must implements");
+	}
+
+	@Override
+	/**
+	 * <h1>Dado un codigo de Asiento compra efectivamente el asiento
+	 */
+	public void comprarAsiento(String codigoAsiento) {
+		// TODO Implementar el codigo
+		
+	}
 
 }
