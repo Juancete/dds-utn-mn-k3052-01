256044c7fe8949cdbca4b663d230f816daabaa87
juan.contardo@gmail.com <juan.contardo@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-06-11 22:29:06 +0000

    Se implementó soySuperOferta. Queda implementar algunos getters y setters.

--- aterrizar.com/src/ar/com/aterrizar/modelo/Asiento.java
+++ aterrizar.com/src/ar/com/aterrizar/modelo/Asiento.java
@@ -9,18 +9,32 @@ public class Asiento {
 	protected char ubicacion;
 	protected char tipo;
 	protected boolean disponible;
-	protected Aerolinea unaAerolinea;
+	protected Aerolinea aerolinea;
 
+	
+	public Asiento(String unCodigo, BigDecimal unPrecio,char unaUbicacion, char unTipo, boolean estaDisponible, Aerolinea unaAerolinea){
+		this.codigo = unCodigo;
+		this.precio = unPrecio;
+		this.ubicacion = unaUbicacion;
+		this.tipo = unTipo;
+		this.disponible = estaDisponible;
+		this.aerolinea = unaAerolinea;
+	}
+	
 	public Boolean soySuperOferta(){
-//		TODO implementar la condición de super-oferta		
+//		Implementación la condición de super-oferta		
 //		Existen asientos que son considerados super ofertas, estos son aquellos asientos que
 //		cumplen con alguna de las siguientes condiciones
-//		â—� son de primera clase y su precio total es menor a $8000
-//		â—� son de clase ejecutiva y su precio total es menor a $4000
-		if (this.precio.compareTo(new BigDecimal(8000))<0){
-			
-		}
-		throw new RuntimeException();
+//		* son de primera clase y su precio total es menor a $8000
+//		* son de clase ejecutiva y su precio total es menor a $4000
+		return (this.precio.compareTo(new BigDecimal(8000))<0 && this.tipo == 'P')||(this.precio.compareTo(new BigDecimal(4000))<0 && this.tipo == 'E');
+	}
+	public void setCodigo(String unCodigo){
+		this.codigo = unCodigo;
+	}
+	
+	public String getCodigo(){
+		return this.codigo;
 	}
 	
 	public void setPrecio(BigDecimal unPrecio){
@@ -28,9 +42,9 @@ public class Asiento {
 	}
 	
 	public Aerolinea getAerolinea(){
-		return this.unaAerolinea; 
+		return this.aerolinea; 
 	}
 	public void setAerolinea(Aerolinea unaAerolinea){
-		this.unaAerolinea = unaAerolinea;
+		this.aerolinea = unaAerolinea;
 	}
 }
