d09c0beebd54c4be161218b8780e3e79877d0275
juan.contardo@gmail.com <juan.contardo@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-06-11 20:42:25 +0000

    Asiento debe manejar los adaptadores como una clase abstracta Aerolinea de la cual heredan lso adapters para cada uan de las diferentes aerolíneas.

--- aterrizar.com/src/ar/com/aterrizar/modelo/Asiento.java
+++ aterrizar.com/src/ar/com/aterrizar/modelo/Asiento.java
@@ -2,8 +2,6 @@ package ar.com.aterrizar.modelo;
 
 import java.math.BigDecimal;
 
-import ar.com.aterrizar.modelo.adapter.AerolineaLanchitaAdapter;
-
 public class Asiento {
 
 	protected String codigo;
@@ -11,13 +9,14 @@ public class Asiento {
 	protected char ubicacion;
 	protected char tipo;
 	protected boolean disponible;
-	protected AerolineaLanchitaAdapter unaAerolinea;
+	protected Aerolinea unaAerolinea;
 
 	public Boolean soySuperOferta(){
+//		TODO implementar la condición de super-oferta		
 //		Existen asientos que son considerados super ofertas, estos son aquellos asientos que
 //		cumplen con alguna de las siguientes condiciones
-//		● son de primera clase y su precio total es menor a $8000
-//		● son de clase ejecutiva y su precio total es menor a $4000
+//		â—� son de primera clase y su precio total es menor a $8000
+//		â—� son de clase ejecutiva y su precio total es menor a $4000
 		if (this.precio.compareTo(new BigDecimal(8000))<0){
 			
 		}
@@ -28,10 +27,10 @@ public class Asiento {
 		this.precio = unPrecio;
 	}
 	
-	public AerolineaLanchitaAdapter getAerolinea(){
+	public Aerolinea getAerolinea(){
 		return this.unaAerolinea; 
 	}
-	public void setAerolinea(AerolineaLanchitaAdapter unaAerolinea){
+	public void setAerolinea(Aerolinea unaAerolinea){
 		this.unaAerolinea = unaAerolinea;
 	}
 }
