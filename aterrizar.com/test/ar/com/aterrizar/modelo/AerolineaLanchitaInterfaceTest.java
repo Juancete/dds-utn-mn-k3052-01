cf31811e1742e8e02b78bbd82ba4812d247f6258
del0al99@gmail.com <del0al99@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-06-12 02:55:22 +0000

    Se resuelve pasar un String[][] como resultado de un objeto impostor(mock)

--- aterrizar.com/test/ar/com/aterrizar/modelo/AerolineaLanchitaInterfaceTest.java
+++ aterrizar.com/test/ar/com/aterrizar/modelo/AerolineaLanchitaInterfaceTest.java
@@ -1,6 +1,7 @@
 package ar.com.aterrizar.modelo;
 
 import static org.mockito.Mockito.*;
+import org.mockito.invocation.*;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -8,6 +9,7 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.stubbing.Answer;
 
 import com.lanchita.AerolineaLanchita;
 
@@ -18,13 +20,18 @@ public class AerolineaLanchitaInterfaceTest {
 	protected AerolineaLanchitaAdapter aerolineaLanchitaAdapter;
 	protected Busqueda busquedaBALA20121010;
 	protected AerolineaLanchita aerolineaLancitaMock;
+	protected String[][] retornoImpostor= new String[][] {{ "01202022220202-3", "159.90", "P", "V", "D", "" },{ "01202022220123-3", "205.10", "E", "P", "D", "" }};
 	
 	@Before
 	public void setUp(){
 		
 		aerolineaLancitaMock = mock(AerolineaLanchita.class);
 		
-		//when(aerolineaLancitaMock.asientosDisponibles("BUE", "LA", "20121010", null, null, null)).thenAnswer(new String[][] {{ "01202022220202-3", "159.90", "P", "V", "D", "" },{ "01202022220123-3", "205.10", "E", "P", "D", "" }});
+		when(aerolineaLancitaMock.asientosDisponibles("BUE", "LA", "20121010", null, null, null)).thenAnswer(new Answer<String[][]>() {
+																							public String[][] answer(InvocationOnMock invocation) throws Throwable{
+																								return (String[][]) retornoImpostor;
+																							}
+																				});
 		 
 		aerolineaLanchitaAdapter = new AerolineaLanchitaAdapter();
 		aerolineaLanchitaAdapter.setAerolinea(aerolineaLancitaMock);
