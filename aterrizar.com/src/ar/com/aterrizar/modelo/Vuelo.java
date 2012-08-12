960b36ec99b0097c2a828e3aa415a04494dcbe82
juan.contardo@gmail.com <juan.contardo@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-08-12 23:07:35 +0000

    Modificada la clase Vuelo para que funcione con el Equals comparando los códigos del mismo y se agregaron los getters y setters de codigo. Además se agregó la popularidad en un vuelo

--- aterrizar.com/src/ar/com/aterrizar/modelo/Vuelo.java
+++ aterrizar.com/src/ar/com/aterrizar/modelo/Vuelo.java
@@ -11,14 +11,16 @@ public class Vuelo {
 	protected Fecha fechaOrigen;
 	protected Fecha fechaDestino;
 	protected List<Asiento> asientos = new ArrayList<Asiento>();
-	protected String codigo;	
+	private String codigo;
+	private int popularidad;
 
 	public Vuelo(String codigoDeVuelo, String unOrigen, String unDestino, Fecha fechaOrigen, Fecha fechaDestino){
-		this.codigo = codigoDeVuelo;
+		this.setCodigo(codigoDeVuelo);
 		this.origen = unOrigen;
 		this.destino = unDestino;
 		this.fechaOrigen = fechaOrigen;
 		this.fechaDestino = fechaDestino;
+		this.popularidad = 0;
 	}
 	
 	public void agregarUnAsiento (Asiento unAsiento){
@@ -27,8 +29,28 @@ public class Vuelo {
 	
 	@Override
 	public boolean equals(Object obj) {
-		Asiento unAsiento = (Asiento)obj;
-		return this.codigo.equals(unAsiento.codigo);
+		Vuelo unVuelo = (Vuelo)obj;
+		return this.getCodigo().equals(unVuelo.getCodigo());
+	}
+	
+	public void incrementarPopularidad(){
+		this.setPopularidad(new Integer(this.getPopularidad() + 1));
+	}
+
+	public int getPopularidad() {
+		return popularidad;
+	}
+
+	public void setPopularidad(int popularidad) {
+		this.popularidad = popularidad;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
 	}
 }
 
