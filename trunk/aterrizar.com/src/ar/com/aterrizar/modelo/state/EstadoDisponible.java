25ce73186d197fa29f362bfafbdca8ddd132fc6d
iguardines@gmail.com <iguardines@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-08-14 22:49:44 +0000

    Agregado parcial del state pattern.

---
+++ trunk/aterrizar.com/src/ar/com/aterrizar/modelo/state/EstadoDisponible.java
@@ -0,0 +1,20 @@
+package ar.com.aterrizar.modelo.state;
+
+import ar.com.aterrizar.modelo.Asiento;
+import ar.com.aterrizar.modelo.Usuario;
+
+public class EstadoDisponible extends Estado {
+
+	@Override
+	public void comprar(Asiento unAsiento, Usuario unUsuario) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reservar(Asiento unAsiento, Usuario unUsuario) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
