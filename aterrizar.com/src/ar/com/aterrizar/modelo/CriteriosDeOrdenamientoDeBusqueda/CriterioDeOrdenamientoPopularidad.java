9c1494c534e264e1a8c3eb41f0a45446ecdb5fdc
juan.contardo@gmail.com <juan.contardo@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-08-14 23:05:39 +0000

    Se modifica el modo de comparar la popularidad. Directamente opto por una resta en lugar del Integer.compare(int, int) que en algunos OS no funcionan

--- aterrizar.com/src/ar/com/aterrizar/modelo/CriteriosDeOrdenamientoDeBusqueda/CriterioDeOrdenamientoPopularidad.java
+++ aterrizar.com/src/ar/com/aterrizar/modelo/CriteriosDeOrdenamientoDeBusqueda/CriterioDeOrdenamientoPopularidad.java
@@ -8,11 +8,11 @@ public class CriterioDeOrdenamientoPopularidad extends CriterioDeOrdenamiento {
 	public int compare(Asiento unAsiento, Asiento otroAsiento) {
 		if (this.ascendente)
 		{
-			return Integer.compare(unAsiento.getVuelo().getPopularidad(),otroAsiento.getVuelo().getPopularidad());
+			return unAsiento.getVuelo().getPopularidad() - otroAsiento.getVuelo().getPopularidad();
 		}
 		else
 		{
-			return Integer.compare(otroAsiento.getVuelo().getPopularidad(),unAsiento.getVuelo().getPopularidad());
+			return otroAsiento.getVuelo().getPopularidad() - unAsiento.getVuelo().getPopularidad();
 		}
 	}
 
