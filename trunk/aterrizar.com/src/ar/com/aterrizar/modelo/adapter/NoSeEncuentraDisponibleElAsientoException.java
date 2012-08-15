b9c6d5bdf17c41c921cb3d364602ff13bbffdc5d
iguardines@gmail.com <iguardines@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-08-15 01:58:15 +0000

    remove warning.

--- trunk/aterrizar.com/src/ar/com/aterrizar/modelo/adapter/NoSeEncuentraDisponibleElAsientoException.java
+++ trunk/aterrizar.com/src/ar/com/aterrizar/modelo/adapter/NoSeEncuentraDisponibleElAsientoException.java
@@ -1,8 +1,9 @@
 package ar.com.aterrizar.modelo.adapter;
 
 public class NoSeEncuentraDisponibleElAsientoException extends RuntimeException {
-
 	
+	private static final long serialVersionUID = -4068897912877107389L;
+
 	public NoSeEncuentraDisponibleElAsientoException(String descripcion){
 		super(descripcion);
 	}
