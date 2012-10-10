563933a55ab334ff1ab78d5d2479cb2454c2080d
juan.contardo@gmail.com <juan.contardo@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-10-10 13:56:11 +0000

    Se agregó el fuente de Fechas para evitar problemas con la integración en el proyecto el día de la entrega XD

---
+++ aterrizar.com/excepcion/FechaException.java
@@ -0,0 +1,37 @@
+/**UTN FRBA - Tp #1 Manejo de Fechas
+ * 
+ * Diseño de sistemas - Curso k3052
+ * Profesores: fdodino, fscarpa 
+ */
+package com.aterrizar.fecha.excepcion;
+/**
+ * <b>FechaException modela una Exception susceptible a ocurrir durante el manejo de Fechas.</b>
+ *  Excepcion no checkeada
+ * 
+ * @version 1.0 
+ * @author iguardines
+ *
+ */
+public class FechaException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3802001112314689132L;
+
+	public FechaException() {
+		super();
+	}
+
+	public FechaException(String mensaje, Throwable causa) {
+		super(mensaje, causa);		
+	}
+
+	public FechaException(String mensaje) {
+		super(mensaje);
+	}
+
+	public FechaException(Throwable causa) {
+		super(causa);		
+	}
+}
