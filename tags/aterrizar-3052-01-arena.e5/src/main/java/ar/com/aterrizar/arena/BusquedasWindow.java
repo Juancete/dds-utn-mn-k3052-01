5c47355d4902af82d0776c01111bb3f15f15d4c1
juan.contardo@gmail.com <juan.contardo@gmail.com@b60d5a8f-7887-1a3c-6010-ffaf2a8bfed9>
2012-11-25 23:13:07 +0000

    Entregable de Arena Persistent

---
+++ tags/aterrizar-3052-01-arena.e5/src/main/java/ar/com/aterrizar/arena/BusquedasWindow.java
@@ -0,0 +1,80 @@
+package ar.com.aterrizar.arena;
+
+import java.text.SimpleDateFormat;
+
+import org.uqbar.arena.actions.MessageSend;
+import org.uqbar.arena.layout.VerticalLayout;
+import org.uqbar.arena.widgets.Button;
+import org.uqbar.arena.widgets.Label;
+import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.widgets.tables.Column;
+import org.uqbar.arena.widgets.tables.Table;
+import org.uqbar.arena.windows.Window;
+import org.uqbar.arena.windows.WindowOwner;
+
+import com.uqbar.commons.collections.Transformer;
+
+import ar.com.aterrizar.daos.BusquedaDaoCollection;
+import ar.com.aterrizar.modelo.Busqueda;
+
+public class BusquedasWindow extends Window<BusquedaDaoCollection> {
+
+	private static final long serialVersionUID = 1L;
+	public BusquedasWindow(WindowOwner owner, BusquedaDaoCollection model) {
+		super(owner, model);
+	}
+
+
+	@Override
+	public void createContents(Panel mainPanel) {
+		this.setTitle("Aterrizar.com");
+		mainPanel.setLayout(new VerticalLayout());
+		this.setSecondTitle(mainPanel);
+		this.createGrid(mainPanel);
+		this.addActions(mainPanel);
+		
+	}
+	public void setSecondTitle(Panel mainPanel) {
+		InicioWindow owner = (InicioWindow) this.getOwner();
+		new Label(mainPanel).setText("Busquedas para "
+				+ owner.getUsuario().getNombre());
+	}
+	
+	public void createGrid(Panel mainPanel) {
+		Table<Busqueda> table = new Table<Busqueda>(mainPanel, Busqueda.class);
+
+		table.bindItemsToProperty(BusquedaDaoCollection.RESULTS);	
+		
+		Column<Busqueda> origenColumn = new Column<Busqueda>(table);
+		origenColumn.setTitle("Origen");
+		origenColumn.setFixedSize(100);
+		origenColumn
+				.bindContentsToProperty("origen");
+		
+		Column<Busqueda> destinoColumn = new Column<Busqueda>(table);
+		destinoColumn.setTitle("Destino");
+		destinoColumn.setFixedSize(100);
+		destinoColumn
+				.bindContentsToProperty("destino");
+
+		Column<Busqueda> fechaColumn = new Column<Busqueda>(table);
+		fechaColumn.setTitle("Fecha");
+		fechaColumn.setFixedSize(100);
+		fechaColumn
+			.bindContentsToTransformer(new Transformer<Busqueda, String>() {
+			@Override
+			public String transform(Busqueda unaBusqueda) {
+				if (unaBusqueda.getFecha() != null){
+				return new SimpleDateFormat("dd/MM/yyyy").format(
+						unaBusqueda.getFecha().getFecha());}
+				else
+				{return "";}
+			}
+		});
+	}
+	private void addActions(Panel mainPanel) {
+
+		Button cerrar = new Button(mainPanel).setCaption("Cerrar");
+		cerrar.onClick(new MessageSend(this, "close"));
+	}
+}
---
+++ tags/aterrizar-3052-01-arena.e5/src/main/java/ar/com/aterrizar/arena/appmodel/SearchAsientoByExample.java
@@ -0,0 +1,83 @@
+package ar.com.aterrizar.arena.appmodel;
+
+import org.uqbar.commons.model.Home;
+import org.uqbar.commons.model.SearchByExample;
+import org.uqbar.commons.model.UserException;
+
+import com.aterrizar.fecha.modelo.Fecha;
+import com.aterrizar.fecha.modelo.FormatoSimple;
+
+import ar.com.aterrizar.arena.InformationWindow;
+import ar.com.aterrizar.arena.InicioWindow;
+import ar.com.aterrizar.arena.SobreReservaDialog;
+import ar.com.aterrizar.daos.AterrizarCom;
+import ar.com.aterrizar.entidades.Asiento;
+import ar.com.aterrizar.modelo.Busqueda;
+import ar.com.aterrizar.modelo.Usuario;
+import ar.com.aterrizar.modelo.adapter.NoSeEncuentraDisponibleElAsientoException;
+import ar.com.aterrizar.modelo.state.AsientoYaReservadoException;
+import ar.com.aterrizar.modelo.state.EstadoReservado;
+
+public class SearchAsientoByExample extends SearchByExample<Asiento> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Usuario miUsuario;
+	private InicioWindow w;
+
+	public SearchAsientoByExample(Home<Asiento> home, Usuario unUsuario,
+			InicioWindow wOwner) {
+		super(home);
+		this.miUsuario = unUsuario;
+		w = wOwner;
+	}
+
+	public void comprar() {
+		try {
+			this.getSelected().comprar(this.miUsuario);
+			if (this.getSelected().getEstado().getClass().equals(EstadoReservado.class)){
+				AterrizarCom.getInstance().getAsientosReservados().delete(this.getSelected());
+			}
+		} catch (NoSeEncuentraDisponibleElAsientoException e) {
+			throw new UserException(e.getMessage());
+		}
+		Asiento asientoSel = this.getSelected();
+		Asiento unAsiento = new Asiento(asientoSel.getCodigo(), asientoSel.getPrecio(), asientoSel.getUbicacion(), asientoSel.getTipo(), asientoSel.getEstado(), asientoSel.getAerolinea());
+		unAsiento.setVuelo(asientoSel.getVuelo());
+		AterrizarCom.getInstance().getAsientosComprados().create(unAsiento);
+		(new InformationWindow(w, new Usuario(),
+				"Su compra se ha realizado exitosamente.")).open();
+	}
+
+	public void reservar() {
+		try {
+			this.getSelected().reservar(this.miUsuario);
+			Asiento asientoSel = this.getSelected();
+			Asiento unAsiento = new Asiento(asientoSel.getCodigo(), asientoSel.getPrecio(), asientoSel.getUbicacion(), asientoSel.getTipo(), asientoSel.getEstado(), asientoSel.getAerolinea());
+			unAsiento.setVuelo(asientoSel.getVuelo());			
+			AterrizarCom.getInstance().getAsientosReservados().create(unAsiento);
+			(new InformationWindow(w, new Usuario(),
+					"Su reserva se ha realizado exitosamente.")).open();
+		} catch (NoSeEncuentraDisponibleElAsientoException e) {
+			throw new UserException(e.getMessage());
+		} catch (AsientoYaReservadoException e) {
+			new SobreReservaDialog(w, this.getSelected(), miUsuario)
+					.open();
+		}
+	}
+	
+	public void search() {
+		Busqueda busqueda = new Busqueda();
+		if(this.getExample().getOrigen() != null)
+			busqueda.setOrigen(this.getExample().getOrigen());
+		if(this.getExample().getDestino() != null)
+			busqueda.setDestino(this.getExample().getDestino());
+		if(!this.getExample().getFecha().isEmpty())
+			busqueda.setFecha(new Fecha(this.getExample().getFecha(), new FormatoSimple("dd/MM/yyyy")));
+		AterrizarCom.getInstance().getBusquedasRealizadas().create(busqueda);
+		super.search();
+	}
+
+}
